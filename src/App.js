fcff749933042ec4561b77fa58c0ac6727a8c5f0
sandypockets <zac@znacreative.com>
2021-04-18 13:44:04 -0400

    Add StyleToggler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import React, { Component } from "react";
 import './App.css';
-import Map from './components/Map.jsx'
+import Map from './components/Map.jsx';
+import StyleToggle from "./components/StyleToggle";
 
-function App() {
-  return (
-    <div className="App">
-      <Map />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 43.6532,
+      longitude: -79.3832,
+      style: 'mapbox://styles/mapbox/dark-v9'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <StyleToggle app={this} />
+        <Map app={this} />
+      </div>
+    );
+  }
 }
 
 export default App;
