2f7cc9f9f01e2a993cba96d575c8aa4de3d07c06
sandypockets <zac@znacreative.com>
2021-04-18 12:02:15 -0400

    Add Map component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Map from './components/Map.jsx'
 
 function App() {
   return (
     <div className="App">
-
+      <Map />
     </div>
   );
 }
---
+++ src/components/Map.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+  render() {
+    return (
+      <div className='App'>
+        Map!!
+      </div>
+    );
+  }
+}
+
+export default Map;
