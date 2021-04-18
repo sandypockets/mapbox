5e4924cf3d64285d8bf5597283651a5494ba6170
sandypockets <zac@znacreative.com>
2021-04-18 15:01:01 -0400

    Create Search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/Search.css';
+
+class Search extends Component {
+
+  render() {
+    return (
+      <form>
+        <input placeholder='Add place...' />
+      </form>
+    )
+  }
+}
+
+export default Search;
