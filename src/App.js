72b4592c6419ce2a9f15746e9b8e9ba1f7382859
sandypockets <zac@znacreative.com>
2021-04-18 13:54:38 -0400

    Add state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import './App.css';
+import './styles/App.css';
 import Map from './components/Map.jsx';
 import StyleToggle from "./components/StyleToggle";
 
