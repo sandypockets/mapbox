9b93dd53fa01b7b44c2fc91eee366f80f09ccde3
sandypockets <zac@znacreative.com>
2021-04-18 13:04:39 -0400

    Refactor

---
+++ src/helpers/getLatLon.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+
+const getIpAndLocation = () => {
+  axios.get('https://api.ipify.org?format=json')
+    .then((res) => res.data.ip)
+    .then((ip) => {
+      axios.get(`https://freegeoip.app/json/${ip}`)
+        .then((res) => {
+          return {
+            lat: res.data.latitude,
+            lon: res.data.longitude,
+            city: res.data.city
+          };
+        }).then((latLonCity) => console.log(latLonCity))
+    }).catch((err) => console.error(err))
+}
+
+getIpAndLocation()
