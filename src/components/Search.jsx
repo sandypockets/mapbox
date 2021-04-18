e7a3102e38bef9b67e072a3cd148a9da17fe9180
sandypockets <zac@znacreative.com>
2021-04-18 15:19:24 -0400

    Add handleChange to form input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import '../styles/Search.css';
 
 class Search extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: '',
+      isLoading: false
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
 
   render() {
     return (
       <form>
-        <input placeholder='Add place...' />
+        <input value={this.state.value} onChange={this.handleChange} placeholder='Add place...' />
       </form>
     )
   }
