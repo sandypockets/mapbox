f634631efcf007f033892f14d3d1e09984748458
sandypockets <zac@znacreative.com>
2021-04-18 14:28:03 -0400

    Add style buttons using .map

--- src/components/StyleToggle.jsx
+++ src/components/StyleToggle.jsx
@@ -3,9 +3,29 @@ import '../styles/StyleToggle.css';
 
 class StyleToggle extends Component {
   render() {
+
+    const mapStyles = [
+      {
+        name: 'Satellite',
+        url: 'mapbox://styles/mapbox/satellite-v9'
+      },
+      {
+        name: 'Dark',
+        url: 'mapbox://styles/mapbox/dark-v10'
+      },
+      {
+        name: 'Light',
+        url: 'mapbox://styles/mapbox/light-v10'
+      }
+    ]
+
+    const buttons = mapStyles.map(style => {
+      return <button>{style.name}</button>
+    })
+
     return (
       <div className='style-toggler'>
-        {this.props.app.state.style}
+        {buttons}
       </div>
     )
   }
