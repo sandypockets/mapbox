a9079fc78fb180b67840f7f60c80a43e13ad917c
sandypockets <zac@znacreative.com>
2021-04-18 22:39:20 -0400

    Set up marker pin and popup

--- src/components/PlaceItem.jsx
+++ src/components/PlaceItem.jsx
@@ -1,13 +1,30 @@
 import React, { Component } from "react";
+import mapbox from 'mapbox-gl';
 import '../styles/PlaceItem.css';
 
 class PlaceItem extends Component {
-
   render() {
+    const app = this.props.app;
+    const map = app.state.map;
+    const place = this.props.place;
+
+    if (map) {
+      const popup = new mapbox.Popup({
+        closeButton: false,
+        className: 'popup'
+      });
+      popup.setHTML(place.title);
+      const marker = new mapbox.Marker({
+        color: '#2727e6'
+      });
+      marker.setLngLat([place.longitude, place.latitude]);
+      marker.setPopup(popup);
+      marker.addTo(map);
+    }
 
     return(
       <div className='place-item'>
-        {this.props.place.name} ({this.props.place.latitude}, {this.props.place.longitude})
+        {place.name} ({place.latitude}, {place.longitude})
       </div>
     )
   }
